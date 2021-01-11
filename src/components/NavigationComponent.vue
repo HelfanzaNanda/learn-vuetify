42a4f222da514134b2007dd4c396e436c3b208a4
HelfanzaNanda <elvannnd@gmail.com>
2021-01-11 11:51:15 +0700

    first commit

---
+++ src/components/NavigationComponent.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <v-app-bar flat app color="blue-grey lighten-4">
+            <v-app-bar-nav-icon
+                class="blue-grey--text d-none d-md-flex"
+                @click="drawer = !drawer"
+            ></v-app-bar-nav-icon>
+            <v-toolbar-title class="text-uppercase blue-grey--text">
+                <span class="font-weight-light">E</span>
+                <span> Maji</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn
+                depressed
+                color="blue-grey lighten-4 blue-grey--text d-none d-md-flex"
+            >
+                <span>sign out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-app-bar>
+
+        <v-navigation-drawer
+            app v-model="drawer"
+            class="blue-grey darken-2 d-none d-md-flex"
+        >
+            <v-list class="mt-16">
+                <v-list-item-group>
+                    <v-list-item
+                        v-for="(item, i) in items"
+                        :key="i"
+                        router
+                        :to="item.route"
+                    >
+                        <v-list-item-icon>
+                            <v-icon
+                                class="white--text"
+                                v-text="item.icon"
+                            ></v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <v-list-item-title
+                                class="white--text caption"
+                                v-text="item.text"
+                            ></v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-bottom-navigation :value="value" color="blue-grey darken-2" app class="d-flex d-md-none">
+            <v-btn v-for="(item, index) in items" :key="index" router :to="item.route">
+                <span>{{ item.text }}</span>
+                <v-icon>{{ item.icon }}</v-icon>
+            </v-btn>
+
+            <v-btn router to="logout">
+                <span>Sign Out</span>
+                <v-icon>exit_to_app</v-icon>
+            </v-btn>
+        </v-bottom-navigation>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            value : 1,
+            drawer: false,
+            items: [
+                { icon: "dashboard", text: "Dashboard", route: "/" },
+                { icon: "next_week", text: "Tool", route: "/tool" },
+                { icon: "people", text: "User", route: "/user" },
+                { icon: "person", text: "Profile", route: "/profile" },
+            ],
+        };
+    },
+};
+</script>
---
+++ src/views/user/Index.vue
@@ -0,0 +1,96 @@
+<template>
+    <div>
+        <v-row>
+            <v-col cols="12" sm="12" md="4">
+                <v-card elevation="2" shaped>
+                    <div class="pa-2">
+                        <div class="ml-2 title text-capitalize">
+                            tambah user
+                        </div>
+                        <v-form ref="form" class="pa-2">
+                            <v-text-field
+                                v-model="form.name"
+                                label="Nama"
+                                required
+                            ></v-text-field>
+                            <v-text-field
+                                v-model="form.email"
+                                label="E-mail"
+                                required
+                            ></v-text-field>
+                            <v-select
+                                v-model="form.role"
+                                label="Role"
+                                required
+                                :items="itemRoles"
+                            ></v-select>
+                            <v-btn
+                                small
+                                color="grey darken-2"
+                                class="mr-4 white--text"
+                                @click="validate"
+                                >tambah</v-btn
+                            >
+                            <v-btn small color="warning" class="white--text"
+                                >reset</v-btn
+                            >
+                        </v-form>
+                    </div>
+                </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="12" md="8">
+                <v-card elevation="2" tile>
+                    <div class="pa-3 title text-capitalize">data user</div>
+
+                    <v-card flat class="clas pa-3">
+                        <v-row v-for="(user, index) in users" :key="index">
+                            <v-col cols="12" sm="12" md="3">
+                                <div class="caption grey--text">Nama</div>
+                                <div class="caption">{{ user.name }}</div>
+                            </v-col>
+
+                            <v-col cols="12" sm="12" md="3">
+                                <div class="caption grey--text">Email</div>
+                                <div class="caption" >{{ user.email }}</div>
+                            </v-col>
+
+                            <v-col cols="12" sm="12" md="3">
+                                <div class="caption grey--text">Role</div>
+                                <div class="caption" >{{ user.role }}</div>
+                            </v-col>
+                            <v-col cols="12" sm="12" md="3">
+                                <div class="caption grey--text text-md-center">Action</div>
+                                <div class="text-md-center">
+                                    <v-btn elevation="2" class="fab blue-grey darken-1 mr-2 white--text" fab x-small><v-icon>edit</v-icon></v-btn>
+                                    <v-btn elevation="2" class="fab blue-grey darken-3 white--text" fab x-small><v-icon>delete</v-icon></v-btn>
+                                </div>
+                            </v-col>
+                            <v-divider cols="12"></v-divider>
+                        </v-row>
+                    </v-card>
+                </v-card>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+export default {
+   
+    data: () => ({
+        users: [
+            { name: "admin", email: "admin@example.com", role: "admin" },
+            { name: "pengawas", email: "pengawas@example.com", role: "pengawas", },
+            { name: "penguji", email: "penguji@example.com", role: "penguji" },
+        ],
+        form: { name: "", email: "", role: "" },
+        itemRoles: ["pengawas", "penguji"],
+    }),
+    methods: {
+        validate() {
+            this.$refs.form.validate();
+        },
+    },
+};
+</script>
